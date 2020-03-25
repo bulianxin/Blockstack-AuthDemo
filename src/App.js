b4136f46e6df08f9a4a59227ab92a0d03d4accca
jingleizhang <eri0x@chain.pro>
2020-03-25 21:56:32 +0800

    update

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import Profile from './Profile.js';
 import Signin from './Signin.js';
 import {
-  UserSession,
-  AppConfig
+    UserSession,
+    AppConfig
 } from 'blockstack';
 
 const appConfig = new AppConfig(['store_write', 'publish_data'])
@@ -11,41 +11,41 @@ const userSession = new UserSession({ appConfig: appConfig })
 
 export default class App extends Component {
 
-  constructor(){
-    super()
-    console.log(appConfig)
-    console.log(userSession)
-  }
-
-  handleSignIn(e) {
-    e.preventDefault();
-    userSession.redirectToSignIn();
-  }
-
-  handleSignOut(e) {
-    e.preventDefault();
-    userSession.signUserOut(window.location.origin);
-  }
-
-  render() {
-    return (
-      <div className="site-wrapper">
-        <div className="site-wrapper-inner">
-          { !userSession.isUserSignedIn() ?
-            <Signin userSession={userSession} handleSignIn={ this.handleSignIn } />
-            : <Profile userSession={userSession} handleSignOut={ this.handleSignOut } />
-          }
-        </div>
-      </div>
-    );
-  }
-
-  componentDidMount() {
-    if (userSession.isSignInPending()) {
-      userSession.handlePendingSignIn().then((userData) => {
-        window.history.replaceState({}, document.title, "/")
-        this.setState({ userData: userData})
-      });
+    constructor() {
+        super()
+        console.log(appConfig)
+        console.log(userSession)
     }
-  }
+
+    handleSignIn(e) {
+        e.preventDefault();
+        userSession.redirectToSignIn();
+    }
+
+    handleSignOut(e) {
+        e.preventDefault();
+        userSession.signUserOut(window.location.origin);
+    }
+
+    render() {
+        return (
+            <div className="site-wrapper">
+                <div className="site-wrapper-inner">
+                    {!userSession.isUserSignedIn() ?
+                        <Signin userSession={userSession} handleSignIn={this.handleSignIn} />
+                        : <Profile userSession={userSession} handleSignOut={this.handleSignOut} />
+                    }
+                </div>
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        if (userSession.isSignInPending()) {
+            userSession.handlePendingSignIn().then((userData) => {
+                window.history.replaceState({}, document.title, "/")
+                this.setState({ userData: userData })
+            });
+        }
+    }
 }
